31d71077d79434902e8ccb194f681bb26722bbe9
Betselot Kidane <betselotkidane111@gmail.com>
2023-04-10 01:47:20 +0300

    initial commit

---
+++ assets/js/meyawo.js
@@ -0,0 +1,38 @@
+// smooth scroll
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// navbar toggle
+$("#nav-toggle").click(function () {
+  $(this).toggleClass("is-active");
+  $("ul.nav").toggleClass("show");
+});
+
+// Portfolio filter
+var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+
+  $("#portfolio-filter li").on("click", function () {
+    $("#portfolio-filter li").removeClass("filter-active");
+    $(this).addClass("filter-active");
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
